dbe9765a0f54f17129d7f26672865d59036beb14
André Dargains <andre.dargains@gmail.com>
2024-05-31 17:49:35 +0100

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { AppWrapper } from "@/context";
 import "./globals.css";
 import "./reset.css";
+import Header from "@/components/Header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AppWrapper>
+          <Header />
           <main>{children}</main>
         </AppWrapper>
       </body>
