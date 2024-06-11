648365c01bae058c49561a1cf11f58e13b385a48
André Dargains <andre.dargains@vodafone.com>
2024-06-11 20:42:04 +0100

    search festival by query

--- src/app/api/festivals/route.ts
+++ src/app/api/festivals/route.ts
@@ -1,11 +1,15 @@
 import { cache, cmsUrl } from "@/helpers/functions";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-const GET = async () => {
+const GET = async (request: NextRequest) => {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+
   const response = await fetch(
-    `${cmsUrl}/items/festivals?filter[status][_eq]=published`,
+    `${cmsUrl}/items/festivals?filter[status][_eq]=published${query ? `&filter[name][_icontains]=${query}` : ""}`,
     { headers: { cache } }
   );
+
   const parsedData = await response.json();
   return NextResponse.json(parsedData);
 };
