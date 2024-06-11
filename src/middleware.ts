c450e42937f81df6673108555fbf24fe53860c25
André Dargains <andre.dargains@vodafone.com>
2024-06-11 22:55:49 +0100

    refactor middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,14 @@ import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const token = cookies().has("token");
-  if (request.nextUrl.pathname === "/profile" && !token) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-  if (request.nextUrl.pathname === "/login" && token) {
-    return NextResponse.redirect(new URL("/profile", request.url));
+  switch (request.nextUrl.pathname) {
+    case "/profile":
+      if (!token) return NextResponse.redirect(new URL("/login", request.url));
+      break;
+    case "/login":
+      if (token) return NextResponse.redirect(new URL("/profile", request.url));
+      break;
+    default:
+      return NextResponse.next();
   }
 }
